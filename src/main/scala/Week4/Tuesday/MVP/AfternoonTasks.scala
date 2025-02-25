3726e37b1cca9a43bb4bb8af9b228ce7912b864f
Dorothea Toeller <dorothea.toeller@mac.home>
2025-02-25 15:09:12 +0000

    task 1 and 2a

---
+++ src/main/scala/Week4/Tuesday/MVP/AfternoonTasks.scala
@@ -0,0 +1,24 @@
+package Week4.Tuesday.MVP
+
+object AfternoonTasks extends App {
+
+  //1 a. Create a function called `intDoubler` that takes an input of type `Int` and returns an `Int`. This method will return the input `Int` doubled.
+  def intDoubler(input: Int): Int = input * 2
+
+  //b. Create a second function called ‘applyTwice’ that takes two parameters.
+  //i. The first parameters is an `input:Int`.
+  //ii. The second parameter is a function called `f` that takes an `Int` and returns an `Int`. (Having this as parameter makes the `applyTwice` method a higher order function.)
+  //iii. The `applyTwice` function must apply `f` twice in succession to the `input`.
+  def applyTwice(input: Int, f: Int => Int): Int = f(input) * 2
+
+  //c. Call the `applyTwice` function passing in the ` intDoubler ` as your `f`. (E.g. applyTwice(4, intDoubler) should equal 16)
+  println(applyTwice(4, intDoubler))
+
+  //2 a. Create 4 functions (addition, multiplication, division, subtraction), each take two doubles as inputs and return a Double as the result.
+  def addition(x: Double, y: Double): Double = x + y
+  def subtraction(x: Double, y: Double): Double = x - y
+  def multiplication(x: Double, y: Double): Double = x * y
+  def division(x: Double, y: Double): Double = x / y
+  //b. Create a function called 'IntManipulator' that takes in two doubles, and a mathematical operation (as made above) as a function and returns the result as a double.
+
+}
