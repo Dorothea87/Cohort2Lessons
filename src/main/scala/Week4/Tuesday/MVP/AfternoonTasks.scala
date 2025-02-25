237717a1504d6c93750cef8021b715518f010225
Dorothea Toeller <dorothea.toeller@mac.home>
2025-02-25 16:44:51 +0000

    finishes task 2

--- src/main/scala/Week4/Tuesday/MVP/AfternoonTasks.scala
+++ src/main/scala/Week4/Tuesday/MVP/AfternoonTasks.scala
@@ -13,12 +13,21 @@ object AfternoonTasks extends App {
 
   //c. Call the `applyTwice` function passing in the ` intDoubler ` as your `f`. (E.g. applyTwice(4, intDoubler) should equal 16)
   println(applyTwice(4, intDoubler))
+  println(applyTwice(100, intDoubler))
 
   //2 a. Create 4 functions (addition, multiplication, division, subtraction), each take two doubles as inputs and return a Double as the result.
   def addition(x: Double, y: Double): Double = x + y
   def subtraction(x: Double, y: Double): Double = x - y
   def multiplication(x: Double, y: Double): Double = x * y
   def division(x: Double, y: Double): Double = x / y
+
   //b. Create a function called 'IntManipulator' that takes in two doubles, and a mathematical operation (as made above) as a function and returns the result as a double.
+  def intManipulator(num1: Double, num2: Double, function: (Double,Double) => Double): Double = function(num1, num2)
+
+  println(intManipulator(5.0, 3.0, addition))
+  println(intManipulator(5.0, 3.0, subtraction))
+  println(intManipulator(5.0, 3.0, multiplication))
+  println(intManipulator(5.0, 3.0, division))
+
 
 }
